89ee342977085f0c3543ac9f620dc63aa49eaec5
tanvirulislam <tanvirul.cse.diu@gmail.com>
2024-07-03 22:01:44 +0600

    change file name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:searchable_dropdown/searchable.dropdown/overlay.entry.dart';
+import 'package:searchable_dropdown/searchable.dropdown/searchable.dropdown.dart.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
